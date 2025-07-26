5f09ce144d1e7ae251ec9cad6ce7524beaf42165
Denys Kibalenko <dennieking87@gmail.com>
2025-07-26 18:03:08 +0200

    Feat: add `ArgsParser` class with `from_args` factory method
    -  Set up `argparse.ArgumentParser` object in `from_args` method, add arguments & return `ArgsParser` instance pupulated with parsed args

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from logging import Logger
 from pathlib import Path
 
@@ -16,6 +17,29 @@ class ArgsParser:
         self.interval = interval
         self.sync_amount = sync_amount
         self.log_file =log_file
+        
+
+    @classmethod
+    def from_args(cls):
+        parser = argparse.ArgumentParser(
+            description="Synchronizer's argument parser"
+        )
+
+        parser.add_argument("source", help="The path to source folder")
+        parser.add_argument("replica", help="The path to replica folder")
+        parser.add_argument("interval", type=int, help="Time between syncs")
+        parser.add_argument("sync_amount", type=int, help="Number of sync runs")
+        parser.add_argument("log_file", help="The path to log file")
+
+        args = parser.parse_args()
+
+        return cls(
+            source=Path(args.source),
+            replica=Path(args.replica),
+            interval=args.interval,
+            sync_amount=args.sync_amount,
+            log_file=Path(args.log_file)
+        )
 
 
 def get_logger(name: str) -> Logger:
@@ -31,7 +55,7 @@ class DirectorySynchronizer:
 
 
 def main():
-    pass
+    args_parser = ArgsParser.from_args()
 
 
 if __name__ == "__main__":
