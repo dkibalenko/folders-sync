f1d53be047b350c97f3fbcb087a20149e389794e
Denys Kibalenko <dennieking87@gmail.com>
2025-07-27 13:22:46 +0200

    Feat: add `DirectorySynchronizer` functionality for synchronization`

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import argparse
+import filecmp
 import logging
 import os
 from pathlib import Path
-from typing import Self
+import shutil
+from typing import Iterator, Self
 
 
 class ArgsParser:
@@ -92,29 +94,83 @@ def get_logger(
     return logger
 
 
-class DirectoryComparator:
-    pass
-
-
 class DirectorySynchronizer:
-    def __init__(self, source: Path, replica: Path):
+    def __init__(self, source: Path, replica: Path, logger: logging.Logger):
         self.source_root = source
         self.replica_root = replica
+        self.logger = logger
 
-    def walk_source(self) -> list[Path]:
-        items_to_sync = []
-        # get all items from source root and sub dirs
-        for root, dirs, files in os.walk(self.source_root):
+    def sync(self) -> None:
+        self._sync_files()
+        self._remove_extra_replica_items()
+
+
+    @staticmethod
+    def _walk_directory_gen(dir_path: Path, topdown=True) -> Iterator[Path]:
+        # iterate recursively from root dir
+        for root, dirs, files in os.walk(dir_path, topdown=topdown):
             for directory in dirs:
-                items_to_sync.append(os.path.join(root, directory))
+                yield Path(root) / directory
             for file in files:
-                items_to_sync.append(os.path.join(root, file))
+                yield Path(root) / file
 
-        return items_to_sync
+    def _sync_files(self) -> None:
+
+        source_paths = self._walk_directory_gen(self.source_root)
+
+        for source_path in source_paths:
+            replica_path = self.replica_root / source_path.relative_to(
+                self.source_root
+            )
+
+            if source_path.is_dir():
+                replica_path.mkdir(parents=True, exist_ok=True)
+                self.logger.info(f"Created directory: {replica_path}")
+            else:
+                is_same_file = filecmp.cmp(
+                    source_path, replica_path, shallow=False
+                )
+
+                if not replica_path.exists() or not is_same_file:
+                    shutil.copy2(source_path, replica_path)
+                    self.logger.info(
+                        f"Copied file: {source_path} -> {replica_path}"
+                    )
+
+    def _remove_extra_replica_items(self) -> None:
+
+        replica_paths = self._walk_directory_gen(
+            self.replica_root,
+            topdown=False
+        )
+
+        for replica_path in replica_paths:
+            source_path = self.source_root / replica_path.relative_to(
+                self.replica_root
+            )
+
+            if not source_path.exists():
+                # delere replica path
+                if replica_path.is_dir():
+                    shutil.rmtree(replica_path)
+                    self.logger.info(f"Removed directory: {replica_path}")
+                else:
+                    replica_path.unlink()
+                    self.logger.info(f"Removed file: {replica_path}")
+        
 
 
 def main():
     args_parser = ArgsParser.from_args()
+    logger = get_logger("Directory Synchronizer", args_parser.log_file)
+    synchronizer = DirectorySynchronizer(
+        args_parser.source,
+        args_parser.replica,
+        logger
+    )
+    import pdb; pdb.set_trace()
+    synchronizer.sync()
+
 
 
 if __name__ == "__main__":
